771763189cf017421233c7d6e1f4421a33271916
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-25 15:11:37 +0900

    feat: base 추가

---
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.tukorea.whereareu.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.tukorea.whereareu.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
