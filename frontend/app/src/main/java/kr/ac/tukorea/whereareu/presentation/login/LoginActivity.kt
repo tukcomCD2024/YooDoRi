4c34ed4dec92254020282e6e331a85a18e343fb3
KKJ <kyujin0911@tukorea.ac.kr>
2024-01-26 01:17:42 +0900

    feat: 사용자 연결 기능 보호자 UI 구성

---
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.whereareu.presentation.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kr.ac.tukorea.whereareu.databinding.ActivityLoginBinding
+import kr.ac.tukorea.whereareu.presentation.base.BaseActivity
+
+class LoginActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val splashScreen = installSplashScreen()
+        setContentView(binding.root)
+    }
+}
