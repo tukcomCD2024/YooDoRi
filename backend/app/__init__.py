f1d55e87c27474516934e06b16ad0cb1be205d20
박승호 <joinus0409@naver.com>
2024-01-26 13:52:50 +0900

    feat: 실시간 위치 정보 및 센서 정보 수집 구현

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .config import Config
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:000409@127.0.0.1:3306/testdb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(Config)
 
     # 다른 설정 및 확장 추가 가능
 
     db.init_app(app)
 
-    from .routes import nok_info_routes, dementia_info_routes
+    from .routes import nok_info_routes, dementia_info_routes, location_info_routes
     app.register_blueprint(nok_info_routes)
     app.register_blueprint(dementia_info_routes)
+    app.register_blueprint(location_info_routes)
 
     return app
