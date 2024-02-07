5f72bb386e69b45a882d94b268d8fc1f92d60778
epe12345 <132982056+epe12345@users.noreply.github.com>
2024-02-07 17:30:26 +0900

    refactor: 예측 모델 개선
    - 모델 정확도 개선

--- ai/app.py
+++ ai/app.py
@@ -5,7 +5,7 @@ from io import StringIO
 import pickle
 
 
-# 저장된 모델 불러오기
+# 저장된 예측 모델 불러오기
 model_filename = 'decision_tree_model.pkl'
 with open(model_filename, 'rb') as model_file:
     model = pickle.load(model_file)
@@ -20,7 +20,7 @@ def predict(data):
 # 전처리 함수
 def preprocessing(data):
     # 클라이언트에서 넘어온 데이터를 입력으로 사용
-
+    # ,로 구분된 한 줄 짜리 데이터
     data = pd.read_csv(StringIO(data), header=None, names=['시간', 'x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3'])
     data = data.drop(['시간'], axis=1)
 
