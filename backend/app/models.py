2cf6c53926d1b9578df21256a11c4e6f5a09d636
박승호 <joinus0409@naver.com>
2024-02-23 02:32:02 +0900

    fix: location_info 중 current speed 삭제

--- backend/app/models.py
+++ backend/app/models.py
@@ -25,7 +25,6 @@ class location_info(db.Model):
     latitude = db.Column(db.Double)
     longitude = db.Column(db.Double)
     user_status = db.Column(db.Integer) # 1: 정지, 2: 도보, 3: 차량, 4: 지하철
-    current_speed = db.Column(db.Float)
     accelerationsensor_x = db.Column(db.Float)
     accelerationsensor_y = db.Column(db.Float)
     accelerationsensor_z = db.Column(db.Float)
