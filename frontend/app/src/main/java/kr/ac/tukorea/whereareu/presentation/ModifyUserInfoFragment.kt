25bddcbd715088c5ad5afd8a12062c8b61da76aa
DoDi <4717jun57@naver.com>
2024-03-25 11:21:39 +0900

    feat: 예외처리
    아무것도 입력하지 않을 경우 메시지 출력, 하나라도 입력한 경우 넘어감

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/ModifyUserInfoFragment.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/ModifyUserInfoFragment.kt
@@ -3,6 +3,7 @@ package kr.ac.tukorea.whereareu.presentation
 import android.content.Context.MODE_PRIVATE
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -18,13 +19,14 @@ import kr.ac.tukorea.whereareu.util.extension.EditTextUtil.setOnEditorActionList
 import kr.ac.tukorea.whereareu.util.extension.EditTextUtil.showKeyboard
 
 @AndroidEntryPoint
-class ModifyUserInfoFragment : BaseFragment<FragmentModifyUserInfoBinding>(R.layout.fragment_modify_user_info) {
-    private val viewModel : SettingViewModel by activityViewModels()
+class ModifyUserInfoFragment :
+    BaseFragment<FragmentModifyUserInfoBinding>(R.layout.fragment_modify_user_info) {
+    private val viewModel: SettingViewModel by activityViewModels()
     private val navigator by lazy {
         findNavController()
     }
 
-    override fun initObserver(){
+    override fun initObserver() {
 
     }
 
@@ -36,69 +38,96 @@ class ModifyUserInfoFragment : BaseFragment<FragmentModifyUserInfoBinding>(R.lay
 
         with(binding) {
             userNameEt.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                if (validUserName()) {
+                val userName = userNameEt.text.toString().trim()
+                if (userName.isBlank()) {
                     userNameEt.error = null
-                    userNameEt.hideKeyboard()
                 } else {
-                    userNameEt.error = "최소 2자의 한글을 입력해주세요"
+                    if (validUserName()) {
+                        userNameEt.error = null
+                        userNameEt.hideKeyboard()
+                    } else {
+                        userNameEt.error = "최소 2자의 한글을 입력해주세요"
+                    }
                 }
             }
             userPhoneEt.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                if (validUserPhone()) {
+                val userPhone = userPhoneEt.text.toString().trim()
+                if (userPhone.isBlank()) {
                     userPhoneEt.error = null
-                    userPhoneEt.hideKeyboard()
                 } else {
-                    userPhoneEt.error = "전화번호 형식이 다릅니다.\n입력 예시) 010-1234-5678"
+                    if (validUserPhone()) {
+                        userPhoneEt.error = null
+                        userPhoneEt.hideKeyboard()
+                    } else {
+                        userPhoneEt.error = "전화번호 형식이 다릅니다.\n입력 예시) 010-1234-5678"
+                    }
                 }
             }
             otherUserNameEt.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                if (validOtherUserName()) {
+                val otherUserName = otherUserNameEt.text.toString().trim()
+                if (otherUserName.isBlank()) {
                     otherUserNameEt.error = null
-                    otherUserNameEt.hideKeyboard()
                 } else {
-                    otherUserNameEt.error = "최소 2자의 한글을 입력해주세요"
+                    if (validOtherUserName()) {
+                        otherUserNameEt.error = null
+                        otherUserNameEt.hideKeyboard()
+                    } else {
+                        otherUserNameEt.error = "최소 2자의 한글을 입력해주세요"
+                    }
                 }
             }
             otherUserPhoneEt.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                if (validOtherUserPhone()) {
+                val otherUserPhone = otherUserPhoneEt.text.toString().trim()
+                if (otherUserPhone.isBlank()){
                     otherUserPhoneEt.error = null
-                    otherUserPhoneEt.hideKeyboard()
                 } else {
-                    otherUserPhoneEt.error = "전화번호 형식이 다릅니다.\n입력 예시) 010-1234-5678"
+                    if (validOtherUserPhone()) {
+                        otherUserPhoneEt.error = null
+                        otherUserPhoneEt.hideKeyboard()
+                    } else {
+                        otherUserPhoneEt.error = "전화번호 형식이 다릅니다.\n입력 예시) 010-1234-5678"
+                    }
                 }
             }
             binding.finishBtn.setOnClickListener {
-                binding.userNameEt.error = if(!validUserName()) "최소 2자의 한글을 입력해주세요" else null
-                binding.otherUserNameEt.error = if(!validOtherUserName()) "최소 2자의 한글을 입력해주세요" else null
-                binding.userPhoneEt.error = if(!validUserPhone()) "전화번호 형식이 다릅니다.\n예시) 010-1234-5678" else null
-                binding.otherUserPhoneEt.error = if(!validOtherUserPhone()) "전화번호 형식이 다릅니다.\n예시) 010-1234-5678" else null
-
-                val userName = binding.userNameEt.text.toString().trim()
-                val userPhone = binding.userPhoneEt.text.toString().trim()
-                val otherUserName = binding.otherUserNameEt.text.toString().trim()
-                val otherUserPhone = binding.otherUserPhoneEt.text.toString().trim()
+                if(userNameEt.text.isBlank() && userPhoneEt.text.isBlank() && otherUserNameEt.text.isBlank() && otherUserPhoneEt.text.isBlank()){
+                    showMessage("변경 사항이 없습니다")
+                    return@setOnClickListener
+                }
+                binding.userNameEt.error = if (!validUserName()) "최소 2자의 한글을 입력해주세요" else null
+                binding.otherUserNameEt.error =
+                    if (!validOtherUserName()) "최소 2자의 한글을 입력해주세요" else null
+                binding.userPhoneEt.error =
+                    if (!validUserPhone()) "전화번호 형식이 다릅니다.\n예시) 010-1234-5678" else null
+                binding.otherUserPhoneEt.error =
+                    if (!validOtherUserPhone()) "전화번호 형식이 다릅니다.\n예시) 010-1234-5678" else null
             }
         }
     }
-    fun onCLickFinishBtn(){
+
+    private fun showMessage(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+    fun onCLickFinishBtn() {
         val spf = requireActivity().getSharedPreferences("User", MODE_PRIVATE)
         viewModel.setUpdateUserName(
-            ModifyUserInfoRequest(0,0, spf.getString("key", ""), binding.userNameEt.text.toString().trim(), "")
+            ModifyUserInfoRequest(0, 0, spf.getString("key", ""), binding.userNameEt.text.toString().trim(), "")
         )
     }
+
     fun onClickBackBtn() {
 //        navigator.popBackStack()
         requireActivity().supportFragmentManager.popBackStack()
     }
 
-    private fun validUserName() = !binding.userNameEt.text.isNullOrBlank()
-            && REGEX_NAME.toRegex().matches(binding.userNameEt.text!!)
-    private fun validUserPhone() = !binding.userPhoneEt.text.isNullOrBlank()
-            && REGEX_PHONE.toRegex().matches(binding.userPhoneEt.text!!)
-    private fun validOtherUserName() = !binding.otherUserNameEt.text.isNullOrBlank()
-            && REGEX_NAME.toRegex().matches(binding.otherUserNameEt.text!!)
-    private fun validOtherUserPhone() = !binding.otherUserPhoneEt.text.isNullOrBlank()
-            && REGEX_PHONE.toRegex().matches(binding.otherUserPhoneEt.text!!)
+    private fun validUserName() = binding.userNameEt.text.isBlank()
+            || REGEX_NAME.toRegex().matches(binding.userNameEt.text!!)
+    private fun validUserPhone() = binding.userPhoneEt.text.isBlank()
+            || REGEX_PHONE.toRegex().matches(binding.userPhoneEt.text!!)
+    private fun validOtherUserName() = binding.otherUserNameEt.text.isBlank()
+            || REGEX_NAME.toRegex().matches(binding.otherUserNameEt.text!!)
+    private fun validOtherUserPhone() = binding.otherUserPhoneEt.text.isBlank()
+            || REGEX_PHONE.toRegex().matches(binding.otherUserPhoneEt.text!!)
 
     companion object {
         private const val REGEX_NAME = "^[가-힣]{2,}\n?"
