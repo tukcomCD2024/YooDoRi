18a36ccdcfab58f1e34b7d469374ec371c9fc07c
박승호 <joinus0409@naver.com>
2024-07-12 22:30:52 +0900

    fix: 이동상태 int -> float, 거리 int -> float

--- backend/fastapi/app/bodymodel.py
+++ backend/fastapi/app/bodymodel.py
@@ -170,8 +170,8 @@ class LocHis(BaseModel):
     latitude : float = Field(examples=["37.123456"])
     longitude : float = Field(examples=["127.123456"])
     time : str = Field(examples=["12:00:00 or 12:00:00,12:01:00"], description='정지 일땐 시작, 끝 시간임')
-    userStatus : int = Field(examples=["1"], description = " 1 : 정지, 2 : 도보, 3 : 차량, 4 : 지하철")
-    distance : int = Field(examples=["2005"], description="미터 단위(소수점 2자리까지)")
+    userStatus : str = Field(examples=["도보"])
+    distance : float = Field(examples=["1.72"], description="미터 단위(소수점 2자리까지)")
 
 class LocHisRecord(BaseModel):
     locationHistory : List[LocHis]
