bffab40cc574638617a3a274ac5de8ee5cf547f7
박승호 <joinus0409@naver.com>
2024-08-29 19:59:16 +0900

    fix: 이동상태 구분 전처리 수정

--- backend/fastapi/app/update_user_status.py
+++ backend/fastapi/app/update_user_status.py
@@ -6,7 +6,7 @@ from .bodymodel import ReceiveLocationRequest
 class UpdateUserStatus:
     def __init__(self):
 
-        model_filename = 'app/random_forest_model_mk2.pkl'
+        model_filename = 'app/random_forest_model_mk4.pkl'
         with open(model_filename, 'rb') as model_file:
             self.model = pickle.load(model_file)
 
@@ -14,22 +14,25 @@ class UpdateUserStatus:
         # 전처리 함수 호출
         preprocessed_data = self.preprocessing(accel, gyro, direction)
         
+        print(preprocessed_data)
         # 모델 예측
         prediction = self.model.predict(preprocessed_data)
+        
+        print(prediction)
         return prediction
 
     def preprocessing(self, accel, gyro, direction):
 
         processed_data = [{
-            'x1': accel[0],
-            'y1': accel[1],
-            'z1': accel[2],
-            'x2': gyro[0],
-            'y2': gyro[1],
-            'z2': gyro[2],
-            'x3': direction[0],
-            'y3': direction[1],
-            'z3': direction[2],
+            'accel.x': accel[0],
+            'accel.y': accel[1],
+            'accel.z': accel[2],
+            'gyro.x': gyro[0],
+            'gyro.y': gyro[1],
+            'gyro.z': gyro[2],
+            'mag.x': direction[0],
+            'mag.y': direction[1],
+            'mag.z': direction[2]
         }]
 
         df = pd.DataFrame(processed_data)
--- backend/fastapi/app/user_status_convertor.py
+++ backend/fastapi/app/user_status_convertor.py
@@ -1,20 +1,20 @@
 def convertor(data):
     
     status = {
-        "정지": 1,
-        "도보": 2,
-        "차량": 3,
-        "지하철": 4
+        "정지": 0,
+        "도보": 1,
+        "차량": 2,
+        "지하철": 3
     }.get(data, None)
 
     return status
     
 def convertor2(data):
     status = {
-        1 : "정지",
-        2 : "도보",
-        3 : "차량",
-        4 : "지하철"
+        0 : "정지",
+        1 : "도보",
+        2 : "차량",
+        3 : "지하철"
     }.get(data, None)
 
     return status
